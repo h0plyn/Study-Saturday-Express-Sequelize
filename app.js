97b39053a6ecb836edf10321155d97693c9b1c27
Ricky Rhodes <ricky@rickyrhodes.com>
2020-11-23 20:36:11 -0500

    add file structure and require db

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const db = require('./db/db')
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const db = require('./db/db');
 
-app.use(bodyParser.json())
-
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 const init = async () => {
   if (require.main === module) {
     //will only run when run with npm start and not with npm test to avoid db syncing in multiple threads when running tests
     try {
-      await db.sync()
+      await db.sync();
       app.listen(3000, () => {
-        console.log('Server is listening on port 3000!')
-      })
+        console.log('Server is listening on port 3000!');
+      });
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
   }
-}
+};
 
-init()
+init();
 
-module.exports = app
+module.exports = app;
